4c8d15be51b92d7f4c616a395c777e1b6b986ef0
KevinmaSchmitz <kevinmaschmitz@gmail.com>
2022-06-22 22:57:08 -0500

    fixed kanye image rotation

--- js/main.js
+++ js/main.js
@@ -12,8 +12,28 @@ function getKanye() {
     });
 }
 
-function newImage() {
-  (document.getElementById("img1").src = "css/Images/New/kanyejawdrop.png")
-}
-document.querySelector('button').addEventListener('click', newImage)
+// function newImage() {
+//   (document.getElementById("img1").src = "css/Images/New/kanyejawdrop.png")
+// }
+// document.querySelector('button').addEventListener('click', newImage)
 
+let imageArr = ['css/Images/New/kanyeAvatar.png', 'css/Images/New/kanyejawdrop.png', 'css/Images/New/kanyephone.png', 'css/Images/New/kanyeserious.png', 'css/Images/New/KanyeTeeth.png']
+// let newImages = imageArr.forEach((i) => document.getElementById('img1').src = imageArr[i])
+
+// function newImages() {
+
+//   for (let i = 0; i < imageArr.length; i++) {
+//     document.getElementById('img1').src = imageArr[i]
+//   }
+// }
+
+
+let counter = 0
+function newImages() {
+  counter += 1
+  if (counter >= imageArr.length) {
+    counter = 0
+  }
+  document.getElementById('img1').src = imageArr[counter]
+}
+document.querySelector('button').addEventListener('click', newImages)
