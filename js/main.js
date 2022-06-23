897dd02fdd527671310d5f8224a24da85c18750a
KevinmaSchmitz <kevinmaschmitz@gmail.com>
2022-06-22 22:57:56 -0500

    cleaned js file

--- js/main.js
+++ js/main.js
@@ -12,21 +12,9 @@ function getKanye() {
     });
 }
 
-// function newImage() {
-//   (document.getElementById("img1").src = "css/Images/New/kanyejawdrop.png")
-// }
-// document.querySelector('button').addEventListener('click', newImage)
+
 
 let imageArr = ['css/Images/New/kanyeAvatar.png', 'css/Images/New/kanyejawdrop.png', 'css/Images/New/kanyephone.png', 'css/Images/New/kanyeserious.png', 'css/Images/New/KanyeTeeth.png']
-// let newImages = imageArr.forEach((i) => document.getElementById('img1').src = imageArr[i])
-
-// function newImages() {
-
-//   for (let i = 0; i < imageArr.length; i++) {
-//     document.getElementById('img1').src = imageArr[i]
-//   }
-// }
-
 
 let counter = 0
 function newImages() {
